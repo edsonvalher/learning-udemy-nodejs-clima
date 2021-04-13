39a2c42d76e621998f21b22d42335d57531dc915
Edson Valenzuela <edsonvalher@gmail.com>
2021-04-13 17:09:05 -0600

    obtener parametros de temperatura manual

--- app.js
+++ app.js
@@ -10,17 +10,24 @@ const main = async () => {
         opt = await inquirerMenu()
         switch (opt) {
             case 1:
+                // mostrar mensaje
                 const termino = await leerInput('Ciudad: ')
+                //selecciona lugar
                 const lugares = await busquedas.ciudad(termino)
                 const id = await listarLugares(lugares)
                 const seleccionado = lugares.find(l => l.id === id)
+                //clima
+                const clima = await busquedas.obtenerClima(seleccionado.lat, seleccionado.lng)
+                //console.log(clima)
+
 
                 console.log(`\nInformación de la ciudad\n`.green)
                 console.log(`Ciudad:`, seleccionado.nombre)
                 console.log(`Lat:`, seleccionado.lat)
                 console.log(`Lng:`, seleccionado.lng)
-                console.log(``)
-                console.log(``)
+                console.log(`Temperatura:`, clima.temp)
+                console.log(`Temperatura Máxima:`, clima.max)
+                console.log(`Temperatura Mínima:`, clima.min)
 
 
 
--- models/busquedas.js
+++ models/busquedas.js
@@ -15,8 +15,6 @@ class Busquedas {
         }
     }
     async ciudad(lugar = '') {
-        //peticion http
-        //console.log(lugar)
         try {
             //const resp = await axios.get("https://api.mapbox.com/geocoding/v5/mapbox.places/Madrid.json?access_token=pk.eyJ1IjoiZWRzb252YWxoZXIiLCJhIjoiY2tuZ2l4b3VtMGJzbTJ3czM3ZXVxaTZrbCJ9.n8zNScoKeSaykySKFBKfRA&limit=5&language=es")
             const instance = axios.create({
@@ -30,13 +28,42 @@ class Busquedas {
                 lng: lugar.center[0],
                 lat: lugar.center[1]
             }))
-            //console.log(resp.data.features)
-            //return [] //retorna los lugares que coincidan coneste lugar            
         } catch (error) {
             return []
         }
+    }
 
+    parametrosClima(lat, lon) {
+        return {
+            'appid': process.env.OPENWHEATHER_KEY,
+            lat,
+            lon,
+            'units': 'metric',
+            'lang': 'es'
+        }
+    }
+    async obtenerClima(lat, lon) {
+        try {
+            //crear instancia axios
+            const instance = axios.create({
+                baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+                params: this.parametrosClima(lat, lon)
+            })
+            const resp = await instance.get()
+            //console.log(resp.data)
+            const { temp, temp_min, temp_max } = resp.data.main
 
+            //resp --> extraer la información  resp.data
+            return {
+                desc: resp.data.weather[0].description, //del main
+                min: temp,
+                max: temp_min,
+                temp: temp_max
+            }
+        } catch (error) {
+            //no se encontro la ciudad
+
+        }
     }
 
 }
